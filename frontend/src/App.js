8d105a4504ed63dec1a4c9896eaae6cf7780634c
WollerChris <99303783+WollerChris@users.noreply.github.com>
2023-07-17 09:16:45 -0600

    Update App.js
    Updated shows a list of hard coded movie data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,25 @@
-import logo from './logo.svg';
+import react from 'react';
 import './App.css';
+
+const movies = [
+  {title: 'Mean Girls'},
+  {title: 'Hackers'},
+  {title: 'The Grey'},
+  {title: 'Sunshine'},
+  {title: 'Ex Machina'},
+];
+
 
 function App() {
   return (
     <div className="App">
+      <h1>Below is the hard coded list of movies</h1>
+      {movies.map(movie => {
+        return (
+        <p>{movie.title}</p>
+        )
+      })}
 
-      <h1> does this work</h1>
     </div>
   );
 }
